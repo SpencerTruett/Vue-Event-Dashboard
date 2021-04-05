8826635e7e6fd67a4d68fdb26825d2652b8eb534
Spencer Truett <srtruett1993@gmail.com>
2021-04-05 11:08:12 -0500

    card displays count of sporting events

--- src/components/SportsCountCard.vue
+++ src/components/SportsCountCard.vue
@@ -1,13 +1,31 @@
 <template>
   <v-app>
-    <v-card></v-card>
+    <v-card>
+      <v-card-subtitle>
+        <div>
+          <v-icon>mdi-football</v-icon>
+          {{ count}}
+        </div>
+        <div class="mt-2">Sporting Events</div>
+      </v-card-subtitle>
+    </v-card>
   </v-app>
 </template>
 
 <script>
-export default {
+import { mapState } from "vuex";
 
-}
+export default {
+  computed: {
+    ...mapState(["sporting"]),
+    count() {
+      if (!this.sporting.meta || !this.sporting.meta.total) {
+        return 0;
+      }
+      return this.sporting.meta.total;
+    }
+  }
+};
 </script>
 
 <style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,27 @@
 <template>
-  <v-container>
-    <div class="mx-auto mt-6">
-      <h1>WELCOME</h1>
-    </div>
-    <div>
-      <DatePicker />
-    </div>
-  </v-container>
+  <v-app>
+    <v-container>
+      <h1>Event Dashboard</h1>
+      <v-col :sm="12" :md="6">
+        <v-row>
+          <v-col>
+            <SportsCountCard />
+          </v-col>
+        </v-row>
+        <div>
+          <DatePicker />
+        </div>
+      </v-col>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
 import DatePicker from "../components/DatePicker"
+import SportsCountCard from "../components/SportsCountCard"
 
 export default {
-  components: { DatePicker }
+  components: { DatePicker, SportsCountCard }
 
 };
 </script>
