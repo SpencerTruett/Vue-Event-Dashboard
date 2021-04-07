91f71a93c45b2e2110bdbba344880d3ab0884094
Spencer Truett <srtruett1993@gmail.com>
2021-04-07 11:13:32 -0500

    can link to /allConcerts to view a longer list

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import AllSports from "../views/AllSports"
 import AllTheaters from "../views/AllTheaters"
+import AllConcerts from "../views/AllConcerts"
 
 Vue.use(VueRouter);
 
@@ -21,6 +22,11 @@ const routes = [
     path: "/allTheaters",
     name: "AllTheaters",
     component: AllTheaters,
+  },
+  {
+    path: "/allConcerts",
+    name: "AllConcerts",
+    component: AllConcerts,
   }
 ];
 
---
+++ src/views/AllConcerts.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-card>
+    <h3>All Concert Event</h3>
+    <v-simple-table>
+      <thead>
+        <tr>
+          <th class="text-left">Event</th>
+          <th class="text-left">Venue</th>
+          <th class="text-left">Date</th>
+          <th class="text-left">Price</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="event in this.concert.events" :key="event.id">
+          <td>{{ event.short_title }}</td>
+          <td>{{ event.venue.name }}</td>
+          <td>{{ new Date(event.datetime_local).toUTCString().split(" ").slice(0, 3).join(" ") }}</td>
+          <td>
+            <a :href="event.url" target="_blank">
+              <div v-if="event.stats.lowest_price != null">
+                ${{event.stats.lowest_price}} -
+                ${{event.stats.highest_price}}
+              </div>
+              <p v-if="event.stats.lowest_price == null">
+                <em>No Tickets</em>
+              </p>
+            </a>
+          </td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </v-card>
+</template>
+
+<script>
+import { mapState } from "vuex";
+
+export default {
+  computed: {
+    ...mapState(["concert"])
+  }
+}
+</script>
+
+<style>
+</style>
